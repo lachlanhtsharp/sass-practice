5f61c59c4bed7476458c994dbb75149f0db5bfc8
Lachlan <lachlanhtsharp@gmail.com>
2021-07-25 14:47:30 -0400

    Practicing Sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { MainPage } from './Components/MainPage';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      Test
+      <Navbar />
+      <MainPage />
     </div>
   );
 }
---
+++ src/Components/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import '../styles/MainPage.css';
+
+export const MainPage = () => {
+    return (
+        <div className='mainPage'>
+            <h1>Welcome to the website</h1>
+            <button>Click Here</button>
+        </div>
+    )
+}
